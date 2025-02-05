a73e72ed2e453fb3003f9592aa9e02818d75a39a
walid-mataich <walidmataich1@gmail.com>
2025-02-05 01:03:27 +0100

    minor fixes

--- sort-view/assets/js/index.js
+++ sort-view/assets/js/index.js
@@ -2,16 +2,21 @@ const infosec = document.querySelector('.info-sec');
 const menu = document.getElementById('menu_btn');
 const nav = document.getElementById('nav-container');
 const cl = document.getElementById('close');
+const blu = document.querySelector('.blur-overlay')
 
 menu.onclick = function() {
 
   nav.style.left = '25vh';
-  nav.style.transition = '0.5s';
+  nav.style.transition = '0.9s';
+  blu.style.visibility = 'visible';
+  blu.style.transition = '0.3s';
+
 
 }
 
 cl.onclick = function() {
   nav.style.left = '100vh';
+  blu.style.visibility = 'hidden';
 }
 
 window.addEventListener('scroll', () => {
